9c7cbb81a403cb3419a3dd061d960337ccb750f6
SamPenDu <ds.schwarzkopf@gmail.com>
2020-02-25 18:55:28 +1300

    25/02/2020 - Release v6.231

--- Various/samsrf_version.m
+++ Various/samsrf_version.m
@@ -7,6 +7,6 @@ function [v,d,o] = samsrf_version
 % The third output o is the SamOaSrf version for Octave support.
 %
 
-v = 6.22; % SamSrf version number
-d = '24-02-2020'; % Release date 
+v = 6.231; % SamSrf version number
+d = '25-02-2020'; % Release date 
 o = 0.1; % SamOaSrf version number
