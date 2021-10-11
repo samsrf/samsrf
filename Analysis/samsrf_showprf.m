67b6732ffb8e27b02d2a8aae7dae20322b896bd6
SamPenDu <ds.schwarzkopf@gmail.com>
2021-10-11 18:05:48 +1300

    v7.513 smaller updates

--- Analysis/samsrf_showprf.m
+++ Analysis/samsrf_showprf.m
@@ -1,28 +1,37 @@
-function samsrf_showprf(SrfEcc, IdxMat, Mode)
+function samsrf_showprf(SrfEcc, IdxMat, Model, PlotType)
 %
-% samsrf_showprf(SrfEcc, IdxMat, [Mode='C'])
+% samsrf_showprf(SrfEcc, IdxMat, Model, [PlotType='C'])
 %
 % Produces a contour or surface plot of a receptive field profile.
-% Use this for displaying reverse correlation pRFs, mean pRFs, or just to
-%  visualise pRF profiles generated with the prf models.
+% Use this for displaying fitted pRF models, reverse correlation pRFs, 
+%  mean pRFs, or just to visualise pRF profiles generated with the models.
 %
-%   SrfEcc:    A Srf structure with reverse correlation data or
-%           for directly plotting a pRF profile matrix, a scalar with the mapping eccentricity.
+%   SrfEcc:    A Srf structure or for directly plotting a pRF profile matrix, 
+%               a scalar with the mapping eccentricity.
 %
 %   IdxMat:    This defines what is to be plotted.
 %               Scalar = Reverse correlation profile for vertex index Idx
-%               Matrix = pRF profile matrix (e.g. from prf_gaussian_rf or samsrf_meanprf)
+%               Matrix = pRF profile matrix (e.g. from prf_gaussian_rf or samsrf_meanprf). 
 %
-%   Mode:   Whether to plot a contour plot ('C'), a surface plot ('S'), or a 3D scatter plot ('D')
+%   Model:     Model structure containing the pRF function etc if that's what your plotting.
+%               If empty this isn't used.
+%
+%   PlotType:  Whether to plot a contour plot ('C'), a surface plot ('S'), or a 3D scatter plot ('D')
+%
 %
 % 19/07/2020 - SamSrf 7 version (DSS)
 % 06/08/2021 - Increase granularity of colour scheme (DSS)
+% 11/10/2021 - Added option to plot pRF models from parameters (DSS)
+%              Changed colour scheme to berlin (DSS) 
 %
 
 if nargin < 3
-    Mode = 'C';
+    Model = [];
 end
-Mode = upper(Mode);
+if nargin < 4
+    PlotType = 'C';
+end
+PlotType = upper(PlotType);
 
 if ~isstruct(SrfEcc) 
     if ~isscalar(SrfEcc)
@@ -39,27 +48,49 @@ if ~isstruct(SrfEcc)
 else
     % Expand Srf if necessary
     SrfEcc = samsrf_expand_srf(SrfEcc);
-    % Reverse correlation plots
-    Xc = SrfEcc.X_coords;
-    Yc = SrfEcc.Y_coords;
-    % Reshape vector into matrix
-    IdxMat = reshape(SrfEcc.Rmaps(:,IdxMat), size(Yc,1), size(Xc,2));
+    if isempty(Model)
+        % Reverse correlation plots
+        Xc = SrfEcc.X_coords;
+        Yc = SrfEcc.Y_coords;
+        % Reshape vector into matrix
+        IdxMat = reshape(SrfEcc.Rmaps(:,IdxMat), size(Yc,1), size(Xc,2));
+    else
+        % Use model parameters
+        if ~isstruct(Model)
+            error('If plotting pRF from model fit, Model structure must be provided!');
+        end
+        P = SrfEcc.Data(2:length(Model.Param_Names)+1, IdxMat); % Fit parameters
+        P(Model.Scaled_Param==1) = P(Model.Scaled_Param==1) / Model.Scaling_Factor; % Rescale parameters if needed
+        IdxMat = Model.Prf_Function(P,200); % Generate pRF profile from parameters
+        % Half width of matrix
+        hw = size(IdxMat,1) / 2;
+        % Coordinate grid
+        [Xc,Yc] = meshgrid([-hw:-1 1:hw]/hw, [-hw:-1 1:hw]/hw);
+        Yc = flipud(Yc); % Because Matlab matrix is stupid
+        % Convert to visual angle
+        Xc = Xc * Model.Scaling_Factor*2;
+        Yc = Yc * Model.Scaling_Factor*2; 
+    end
 end
 
 %% Plot data
-if Mode == 'C'
+if PlotType == 'C'
     contourf(Xc, Yc, IdxMat, 500, 'EdgeColor', 'none');
     axis square
-elseif Mode == 'S'
+elseif PlotType == 'S'
     surf(Xc, Yc, IdxMat, 'EdgeColor', 'none', 'FaceColor', 'interp');
-elseif Mode == 'D'
+elseif PlotType == 'D'
     scatter3(Xc(:), Yc(:), IdxMat(:), 100, IdxMat(:), 'filled');
 else
     error('Unknown plotting mode specified.');
 end
+if isstruct(Model)
+    % If plotting model parameters, we must remove deadspace 
+    axis([-1 +1 -1 +1] * Model.Scaling_Factor);
+end
 Scale = max(abs([nanmin(IdxMat(:)) nanmax(IdxMat(:))]));
 set(gca, 'Clim', [-1 +1]*Scale);
-colormap hotcold
+colormap berlin
 colorbar 
 xlabel('Horizontal position (deg)');
 ylabel('Vertical position (deg)');
--- ReadMe.md
+++ ReadMe.md
@@ -30,10 +30,11 @@ be in the same format. You can also convert old data files from older versions
 
 ## LATEST UPDATES 
 
-### Version 7.512 (08-10-2021)  
+### Version 7.513 (11-10-2021)  
 - New option in samsrf_surf to display data vector directly (DSS)  
 - AutoDelineation now also uses default ROI list if defined (DSS)  
 - Added new skewed Gaussian pRF model (DSS)  
+- New option in samsrf_showprf to plot pRF from model parameters (DSS)  
 - Various minor cosmetic changes (DSS)  
 
 ### Version 7.51 (07-10-2021)  
