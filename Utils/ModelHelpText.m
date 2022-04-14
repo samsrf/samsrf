a5f75f54c2cebb053de1562c45e03f18750ab4b8
SamPenDu <ds.schwarzkopf@gmail.com>
2022-04-15 04:07:04 +1200

    Working version of ModelHelp (I hope)

--- Utils/ModelHelpText.m
+++ Utils/ModelHelpText.m
@@ -14,7 +14,8 @@ switch AnalysisFunc
    case 'samsrf_fit_prf'
        switch ParameterName
            case 'Prf_Function'
-               HelpText = '';
+               HelpText = {'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' 
+                   'in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'};
            case 'Name'
                HelpText = '';
            case 'Param_Names'
